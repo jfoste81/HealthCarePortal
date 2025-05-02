9404341409663c3db498261e3e2499824489725b
jfoste81 <jfoste81@uncc.edu>
2025-05-02 17:08:55 -0400

    Finished new message screen

---
+++ Forms/MessageForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace HealthCarePortal.Forms
+{
+    partial class MessageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxMessage = new System.Windows.Forms.GroupBox();
+            this.labelTo = new System.Windows.Forms.Label();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.labelSubject = new System.Windows.Forms.Label();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.textBoxBody = new System.Windows.Forms.TextBox();
+            this.labelBody = new System.Windows.Forms.Label();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.groupBoxMessage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxMessage
+            // 
+            this.groupBoxMessage.Controls.Add(this.buttonSend);
+            this.groupBoxMessage.Controls.Add(this.labelBody);
+            this.groupBoxMessage.Controls.Add(this.textBoxBody);
+            this.groupBoxMessage.Controls.Add(this.textBoxSubject);
+            this.groupBoxMessage.Controls.Add(this.labelSubject);
+            this.groupBoxMessage.Controls.Add(this.comboBoxTo);
+            this.groupBoxMessage.Controls.Add(this.labelTo);
+            this.groupBoxMessage.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxMessage.Name = "groupBoxMessage";
+            this.groupBoxMessage.Size = new System.Drawing.Size(775, 425);
+            this.groupBoxMessage.TabIndex = 0;
+            this.groupBoxMessage.TabStop = false;
+            this.groupBoxMessage.Text = "Create Message";
+            // 
+            // labelTo
+            // 
+            this.labelTo.AutoSize = true;
+            this.labelTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.labelTo.Location = new System.Drawing.Point(43, 37);
+            this.labelTo.Name = "labelTo";
+            this.labelTo.Size = new System.Drawing.Size(34, 20);
+            this.labelTo.TabIndex = 0;
+            this.labelTo.Text = "To:";
+            // 
+            // comboBoxTo
+            // 
+            this.comboBoxTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Location = new System.Drawing.Point(83, 34);
+            this.comboBoxTo.Name = "comboBoxTo";
+            this.comboBoxTo.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxTo.TabIndex = 1;
+            // 
+            // labelSubject
+            // 
+            this.labelSubject.AutoSize = true;
+            this.labelSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSubject.Location = new System.Drawing.Point(245, 37);
+            this.labelSubject.Name = "labelSubject";
+            this.labelSubject.Size = new System.Drawing.Size(75, 20);
+            this.labelSubject.TabIndex = 2;
+            this.labelSubject.Text = "Subject:";
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxSubject.Location = new System.Drawing.Point(327, 36);
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(391, 26);
+            this.textBoxSubject.TabIndex = 3;
+            // 
+            // textBoxBody
+            // 
+            this.textBoxBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxBody.Location = new System.Drawing.Point(47, 97);
+            this.textBoxBody.Multiline = true;
+            this.textBoxBody.Name = "textBoxBody";
+            this.textBoxBody.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxBody.Size = new System.Drawing.Size(671, 268);
+            this.textBoxBody.TabIndex = 4;
+            // 
+            // labelBody
+            // 
+            this.labelBody.AutoSize = true;
+            this.labelBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBody.Location = new System.Drawing.Point(43, 74);
+            this.labelBody.Name = "labelBody";
+            this.labelBody.Size = new System.Drawing.Size(105, 20);
+            this.labelBody.TabIndex = 5;
+            this.labelBody.Text = "Description:";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSend.Location = new System.Drawing.Point(643, 384);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 26);
+            this.buttonSend.TabIndex = 6;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            // 
+            // MessageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxMessage);
+            this.Name = "MessageForm";
+            this.Text = "MessageForm";
+            this.groupBoxMessage.ResumeLayout(false);
+            this.groupBoxMessage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxMessage;
+        private System.Windows.Forms.TextBox textBoxBody;
+        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.Label labelSubject;
+        private System.Windows.Forms.ComboBox comboBoxTo;
+        private System.Windows.Forms.Label labelTo;
+        private System.Windows.Forms.Label labelBody;
+        private System.Windows.Forms.Button buttonSend;
+    }
+}
---
+++ Forms/MessageForm.cs
@@ -0,0 +1,96 @@
+﻿using HealthcarePortal.Models;
+using HealthCarePortal.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace HealthCarePortal.Forms
+{
+    public partial class MessageForm : Form
+    {
+        private readonly User _sender;
+        private readonly User _fixedRecipient;
+
+        public MessageForm(User sender, User recipient = null, string subject = "")
+        {
+            InitializeComponent();
+            _sender = sender;
+            _fixedRecipient = recipient;
+
+            // Populate the "To" dropdown
+            if (_fixedRecipient != null)
+            {
+                comboBoxTo.Items.Add(_fixedRecipient.Username);
+                comboBoxTo.SelectedIndex = 0;
+                comboBoxTo.Enabled = false;
+            }
+            else
+            {
+                // if sender is a patient, list doctors; if doctor, list patients
+                var choices = sender is Patient
+                ? Portal.Instance.Doctors.Select(d => d.Username)
+                    : Portal.Instance.Patients.Select(p => p.Username);
+
+                comboBoxTo.Items.AddRange(choices.ToArray());
+            }
+
+            // Prefill subject for replies
+            textBoxSubject.Text = subject;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            string toUser = comboBoxTo.SelectedItem?.ToString() ?? "";
+            string subj = textBoxSubject.Text.Trim();
+            string body = textBoxBody.Text.Trim();
+
+            if (string.IsNullOrEmpty(toUser)
+             || string.IsNullOrEmpty(subj)
+             || string.IsNullOrEmpty(body))
+            {
+                MessageBox.Show(
+                  "Please select a recipient, and fill in subject and body.",
+                  "Missing Information",
+                  MessageBoxButtons.OK,
+                  MessageBoxIcon.Warning
+                );
+                return;
+            }
+
+            // Find the recipient object
+            var recipient = (User)Portal.Instance.Doctors
+                                .FirstOrDefault(d => d.Username == toUser)
+                          ?? Portal.Instance.Patients
+                                .FirstOrDefault(p => p.Username == toUser);
+
+            if (recipient == null)
+            {
+                MessageBox.Show(
+                  "Could not find the recipient user.",
+                  "Error",
+                  MessageBoxButtons.OK,
+                  MessageBoxIcon.Error
+                );
+                return;
+            }
+
+            // Create & deliver the message
+            var msg = new UserMessage(_sender.Username, subj, body);
+            recipient.Inbox.Add(msg);
+            recipient.Notifications.Add(new Notification(
+                "Message",
+                $"New message from {_sender.Name}"
+            ));
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
